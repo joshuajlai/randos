bfed86495a80a68dd937bc2ae8288f83daa58fcd
joshuajlai <joshuajlai@gmail.com>
2017-10-06 12:16:54 -0700

    simple script to format html

---
+++ html_formatter.php
@@ -0,0 +1,13 @@
+<?php
+
+if (2 !== $argc) {
+    echo "Missing html input\n";
+    exit(1);
+}
+
+$input = $argv[1];
+$doc = new DomDocument('1.0');
+$doc->loadHTML($input);
+$doc->formatOutput = true;
+
+echo $doc->saveHTML() . "\n";
