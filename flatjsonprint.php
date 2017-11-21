2d088d52670c92270559a60cbbadd441968a083e
joshuajlai <joshuajlai@gmail.com>
2017-11-21 14:51:31 -0800

    adding php scripts or something

---
+++ flatjsonprint.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php
+<?php
+
+if (2 !== $argc) {
+    echo "Need input file\n";
+    exit(1);
+}
+
+$input = file_get_contents($argv[1]);
+echo json_encode(json_decode($input, true)) . "\n";
